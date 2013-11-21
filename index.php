fefb69526a8c959e1ebd79f3b0e8a06ac0ab3d43
Reinhold Hölzgen <reinhold.hoelzgen@gmx.de>
2013-11-21 15:20:54 +0100

    fuer Hausaufgabe

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<title>Willkommen!</title>
+</head>
+<body>
+<header>
+	<h1>Willkommen!</h1>
+</header>
+    <?php
+
+	// Zeigt alle Informationen (Standardwert ist INFO_ALL)
+	phpinfo();
+
+	?>
+
+</body>
+</html>
